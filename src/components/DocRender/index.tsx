d6dec30e2533131d65d17884147b15f1f3e6c005
何柳杰 <268079@163.com>
2023-03-09 23:02:49 +0800

    feat: contentEditable

--- src/components/DocRender/index.tsx
+++ src/components/DocRender/index.tsx
@@ -4,20 +4,33 @@ import useStore from 'doc-processor/store';
 import { readFileAsArrayBuffer } from 'doc-processor/utils';
 import mammoth from 'mammoth/mammoth.browser';
 import React from 'react';
+import ContentEditable from '../ContentEditable';
+// import ReactQuill from '../Quill';
 
 export default function DocRender() {
-  const { docFile } = useStore((state) => state);
+  const { docFile, docHtmlString, uDocHtmlString } = useStore((state) => state);
   useMount(async () => {
     if (!docFile) return;
     try {
       const arrayBuffer = await readFileAsArrayBuffer(docFile);
       const result = await mammoth.convertToHtml({ arrayBuffer } as any);
+      uDocHtmlString(result?.value || '');
       console.log(result);
     } catch (error) {
       message.error('get doc file fail');
       console.log(error);
     }
   });
-  console.log({ docFile, mammoth });
-  return <div>DocRender</div>;
+  return (
+    <>
+      <div>
+        <ContentEditable
+          html={docHtmlString}
+          onChange={(evt) => {
+            uDocHtmlString(evt.target.value);
+          }}
+        />
+      </div>
+    </>
+  );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,17 @@ import { create } from 'zustand';
 interface StoreType {
   docFile: File | null;
   uUploadFile: (...args: any[]) => void;
+
+  docHtmlString: string;
+  uDocHtmlString: (...args: any[]) => void;
 }
 
 const useStore = create<StoreType>((set) => ({
   docFile: null,
   uUploadFile: (file: File) => set(() => ({ docFile: file })),
+
+  docHtmlString: '',
+  uDocHtmlString: (docHtmlString: string) => set(() => ({ docHtmlString })),
 }));
 
 export default useStore;
